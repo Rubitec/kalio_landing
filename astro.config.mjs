cde4f4ece467996a107fde2dcc82ed295916d361
Mateo <mateorubinstein@icloud.com>
2025-01-11 19:11:58 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rubitec.github.io",
+  base: "/kalio_landing/",
   vite: {
     css: {
       preprocessorOptions: {
